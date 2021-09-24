bec9a925c4090797ac89e37a496b50b1ef95145c
Danilo Quilici <daniloquilici@outlook.com>
2021-09-23 23:02:39 -0300

    Padrão Factory

---
+++ Factory/ConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Factory
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection connection = new SqlConnection();
+            connection.ConnectionString = "";
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factory");
+
+            IDbConnection connection = new ConnectionFactory().GetConnection();
+
+            IDbCommand comando = connection.CreateCommand();
+            comando.CommandText = "Select * from usuario;";
+            var usuarios = comando.ExecuteReader();
+        }
+    }
+}
