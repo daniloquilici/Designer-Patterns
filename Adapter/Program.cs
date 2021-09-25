e40ba6fe0cc3521531e064e436b2d7f6ef5873b3
Danilo Quilici <daniloquilici@outlook.com>
2021-09-25 16:53:09 -0300

    Padrão Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adapter");
+
+            var geradorXML = new GerarXML();
+            
+            var cliente = new Cliente();
+            cliente.Nome = "Danilo";
+            cliente.Endereco = "Rua Nelson Gama de Oliveira, 905 Vila Andrade";
+            cliente.DataNascimento = DateTime.Now;
+
+            Console.WriteLine(geradorXML.Gerar(cliente));
+            Console.ReadKey();
+        }
+    }
+}
